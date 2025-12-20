153861f11a3f51636d64ff43a48454d6b7015a5a
rpron <rpron@prominn.ru>
2025-12-20 11:59:42 +0300

    add get cars route

--- app/routes/car.py
+++ app/routes/car.py
@@ -1,13 +1,23 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends
+from sqlalchemy import select
 from typing import List
 
-from app.SAmodels.car import Car as SAModelCar
-from app.schemas.car import CarBase, CarCreate, CarUpdate, CarOut
-from app.SAmodels.database import get_db  
+from app.SAmodels.database import async_session_maker
+from app.SAmodels.car import Car  
+from app.schemas.car import CarOut, CarFilter
 
 router = APIRouter()
 
-@router.get("/", response_model=CarOut)
-def get_cars():
-    return "поршик панамера"
+@router.get("", response_model=List[CarOut])  
+async def get_sales(
+car_filter: CarFilter = Depends()
+):
+    async with async_session_maker() as session:
+        q = select(Car)
+        if car_filter.brand:
+            q = q.where(Car.brand == car_filter.brand)        
+        result = await session.execute(q)
+        cars = result.scalars().all()
+
+        return cars  
+
