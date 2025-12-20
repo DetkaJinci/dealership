0ac635f2d8106ade8074adf13d7712f0c3b52ef4
rpron <rpron@prominn.ru>
2025-12-20 11:59:50 +0300

    add get sales route

--- app/routes/sale.py
+++ app/routes/sale.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends
 from sqlalchemy import select
 from typing import List
-from app.SAmodels.sale import Sale
-from app.schemas.sale import SaleOut, SaleFilter
+
 from app.SAmodels.database import async_session_maker
+from app.SAmodels.sale import Sale  
+from app.schemas.sale import SaleOut, SaleFilter
 
 router = APIRouter()
 
@@ -18,5 +19,10 @@ sale_filter: SaleFilter = Depends()
             q = q.where(Sale.car_id == sale_filter.car_id)        
         result = await session.execute(q)
         sales = result.scalars().all()
+
+        if sale_filter.user_id:
+            q = q.where(Sale.user_id == sale_filter.user_id)
         return sales  
-    
+
+
+            
